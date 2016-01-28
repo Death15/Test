16eb3a3ddadec158b432e4d90a19dba2902e6aa3
mardan15 <arash.official15@gmail.com>
2016-01-28 03:09:13 -0800

    Update Info.lua

--- plugins/Info.lua
+++ plugins/Info.lua
@@ -6,7 +6,7 @@ local function action_by_reply(extra, success, result)
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info.msgs = tonumber(redis:get(um_hash) or 0)
   user_info.name = user_print_name(user)..' ['..result.from.id..']'
-  local msgs = 'Messages sent : '..user_info.msgs
+  local msgs = 'Total Messages : '..user_info.msgs
   if result.from.username then
     user_name = '@'..result.from.username
   else
@@ -19,13 +19,13 @@ local function action_by_reply(extra, success, result)
   local chat = 'chat#id'..msg.to.id
   local data = load_data(_config.moderation.data)
   if data[tostring('admins')][tostring(user_id)] then
-    who = 'Admin'
+    status = 'Admin'
   elseif data[tostring(msg.to.id)]['moderators'][tostring(user_id)] then
-    who = 'Moderator'
+    status = 'Moderator'
   elseif tonumber(result.from.id) == tonumber(our_id) then
-    who = 'Group creator'
+    status = 'Group creator'
   else
-    who = 'Member'
+    status = 'Group Member'
   end
   for v,user in pairs(_config.sudo_users) do
     if user = user_id then
@@ -38,19 +38,20 @@ local function action_by_reply(extra, success, result)
              ..'Username : '..user_name..'\n\n'
              ..'UserI-D : '..result.from.id..'\n\n'
              ..'total Maseages : '..msgs..'\n\n'
-             ..'User Status : '..status
+             ..'User Status : '..status..'\n\n'
+             ..'Group I-D : '..chat_id..'\n\n
   send_large_msg(extra.receiver, text)
 end
 
 local function run(msg)
-   if msg.text == '/info' or msg.text == '!info' and is_momod(msg) then
+   if msg.text == '/infouser' or msg.text == '!infouser' and is_momod(msg) then
       get_message(msg.reply_id, action_by_reply, {receiver=get_receiver(msg)})
    end
 end
 
 return {
     patterns = {
-      '^[!/]info$'
+      '^[!/](infouser)$'
     },
   run = run
 }
